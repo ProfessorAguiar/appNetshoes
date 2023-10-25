ae334ddd21042eaa29a55e0e2d2231eda015761e
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-10-25 19:40:46 -0300

    Aula do dia 25/10 19:40

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  public vestuario=[
+    { 
+      id: 1, 
+      produto: "Camisa Flamengo I 22/23 s/n° Torcedor Adidas Masculina - Vermelho+Preto",
+      preco:"R$149,99",
+      descricao:"A maior paixão do mundo! Prestando homenagem à emoção que pulsa nas arquibancadas quando o Mengo está em campo, a nova Camisa Flamengo Masculina da Adidas chega com as tradicionais listras pretas e vermelhas, mas, desta vez, sobrepostas por linhas finas vermelhas onduladas na horizontal que remetem ao movimento das bandeiras dos torcedores. Confeccionada com tecido leve e altamente respirável, o modelo torcedor do manto titular ainda carrega o brasão rubro-negro bordado na altura do peito. Dentro ou fora do Maracanã, exiba o orgulho de ser Mengão usando a Camisa Masculina do Flamengo!",
+      miniDescricao:"A maior paixão do mundo! Prestando homenagem à emoção que pulsa nas arquibancadas quando o Mengo.",
+      imagem:"https://static.netshoes.com.br/produtos/camisa-flamengo-i-2223-sn-torcedor-adidas-masculina/68/3ZP-5382-068/3ZP-5382-068_zoom1.jpg" 
+  },
+]
+
   constructor() {}
 
 }
