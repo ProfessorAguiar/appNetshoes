0be0280e3e340825956ceca87d184393bdf60829
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-10-27 21:33:44 -0300

    Inicio do firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  mostrarApp=false
+  c=false
+  mensagem=''
+  login={user:'admin',pass:123456}
+  fazerLogin(usuario:any, senha:any){
+    this.mensagem=''
+    console.log(`fazendo login do usuário: ${usuario} com senha: ${senha}`)
+    if(usuario==this.login.user && senha==this.login.pass){
+      this.mostrarApp=true
+    }else{
+      this.mensagem='Usuário não cadastrado ou senha inválida!'
+    }
+  }
+
+  Cadastrar(usuario:any, senha:any){
+    this.mensagem=''
+    console.log(`fazendo login do usuário: ${usuario} com senha: ${senha}`)
+    if(usuario!='' && senha!=''){
+      this.c=false
+    }else{
+      this.mensagem='Digite em nome de Usuário e uma senha!'
+    }
+  }
+  mostrarCadastrar(){
+    this.mensagem=''
+    this.c=true
+  }
   constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,47 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNWEjx5OyfoercifthlvjQkJXDnF-z9ok",
+  authDomain: "netshoes-43932.firebaseapp.com",
+  projectId: "netshoes-43932",
+  storageBucket: "netshoes-43932.appspot.com",
+  messagingSenderId: "59072359974",
+  appId: "1:59072359974:web:2865743af1ba5f8e0c80e7",
+  measurementId: "G-R9BKFGB3KH"
+};
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCNWEjx5OyfoercifthlvjQkJXDnF-z9ok",
+//   authDomain: "netshoes-43932.firebaseapp.com",
+//   projectId: "netshoes-43932",
+//   storageBucket: "netshoes-43932.appspot.com",
+//   messagingSenderId: "59072359974",
+//   appId: "1:59072359974:web:2865743af1ba5f8e0c80e7",
+//   measurementId: "G-R9BKFGB3KH"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
